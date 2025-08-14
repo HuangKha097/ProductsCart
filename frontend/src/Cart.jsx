00961748614c9bb5ae4f7a9db4cd7f2fae3994fe
HuangKha097 <huangkha097@gmail.com>
2025-08-14 17:50:47 +0700

    Finish

--- frontend/src/Cart.jsx
+++ frontend/src/Cart.jsx
@@ -13,7 +13,6 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                 : alert("Empty Cart");
         }
     };
-    console.log(isOrderConfirmed);
 
     return (
         <div className={cx("container")}>
@@ -48,9 +47,9 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                                 />
                                 <path
                                     stroke="#fff"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width=".974"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth=".974"
                                     d="m81.076 28.966 34.187-4.16"
                                 />
                                 <path
@@ -71,9 +70,9 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                                 />
                                 <path
                                     stroke="#fff"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width=".974"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth=".974"
                                     d="M9.796 52.06c-.667 5.866 16.24 12.586 37.733 15.04 14.774 1.68 27.867.906 34.854-1.654"
                                 />
                             </svg>
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../src/css/Home.module.scss";
 import className from "classnames/bind";
 import ProductList from "./ProductList";
 import Cart from "./Cart";
-import OrderConfirmed from "./OrderConfirmed";
 
 const cx = className.bind(styles);
 
 const Home = () => {
     const [cart, setCart] = useState([]);
+    const [isShowScroll, setShowScrollTop] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setShowScrollTop(true);
+            } else {
+                setShowScrollTop(false);
+            }
+        });
+    }, []);
 
     const handleAddToCart = (item) => {
         setCart([...cart, item]);
@@ -20,6 +30,7 @@ const Home = () => {
             setCart(cart.filter((_, i) => i !== index));
         }
     };
+    console.log(isShowScroll);
 
     return (
         <div className={cx("content")}>
@@ -38,6 +49,29 @@ const Home = () => {
                     />
                 </div>
             </div>
+            {isShowScroll && (
+                <button
+                    className={cx("scroll-to-top")}
+                    onClick={() =>
+                        window.scrollTo({ top: 0, behavior: "smooth" })
+                    }
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                        />
+                    </svg>
+                </button>
+            )}
         </div>
     );
 };
